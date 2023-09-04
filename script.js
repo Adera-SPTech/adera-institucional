5da6200f3aae32d9b87a84f915761ba7cac22e94
Renan silva <renansilva.dev@gmail.com>
2023-09-04 19:37:55 -0300

    Primeira sessão pronta, juntamente com ajustes nomenu de navegação

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hamburguerButton = document.getElementById('hamburguer-button')
+const blurDiv = document.getElementById('blur-div')
 const menu = document.getElementById('menu')
 hamburguerButton.addEventListener('click', (e) => {
   menu.classList.toggle('open')
+  blurDiv.classList.toggle('shown')
 })
 
-menu.addEventListener('click', (e) => {
+blurDiv.addEventListener('click', (e) => {
   menu.classList.toggle('open')
+  blurDiv.classList.toggle('shown')
 })
